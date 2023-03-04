8ee3d917fc08b637d2ef741aa096406332e5fb1a
Sławek Witek <slawekwitek@icloud.com>
2023-03-04 18:09:23 +0100

    Zmiana tekstu powiadomienia email

--- common/untils.py
+++ common/untils.py
@@ -5,13 +5,13 @@ from django.core.mail import send_mail
 def send_my_email(dostawca, user_email, user_firstname, user_lastname):
     tytul_email = f'Dostawa {user_lastname} - {dostawca}'
     message = """
-                            Witam!!
+    Witam!!
 
-                            Dodano nową dostawę z %s na stacji paliw %s.
-                            Wejdź i sprawdź https://slawekwitek.smallhost.pl/
-                            Miłego dnia :)
-                            Dostawę dodał/a użytkownik - %s .
-                            """ % (dostawca, user_lastname, user_firstname)
+    Dodano nową dostawę z %s na stacji paliw %s.
+    Wejdź i sprawdź https://slawekwitek.smallhost.pl/
+    Miłego dnia :)
+    Dostawę dodał/a użytkownik - %s .
+    """ % (dostawca, user_lastname, user_firstname)
     responder_email = os.environ.get('EMAIL_HOST_USER')
     admin_address = os.environ.get('ADMIN_ADDRES')
     email_two = user_email
@@ -21,13 +21,13 @@ def send_my_email(dostawca, user_email, user_firstname, user_lastname):
 def send_my_email_modified(dostawa_number, user_email, user_firstname, user_lastname):
     tytul_email = f'Uwaga zmodyfikowano dostawę numer {dostawa_number} stacja {user_lastname}'
     message = """
-                           Uwaga!!
+    Uwaga!!
 
-                            Zmodyfikowano dostawę numer %s na stacji paliw %s .
-                            Wejdź i sprawdź https://slawekwitek.smallhost.pl/
-                            Miłego dnia :)
-                            Dostawę edytował użytkownik - %s .
-                            """ % (dostawa_number, user_lastname, user_firstname)
+    Zmodyfikowano dostawę numer %s na stacji paliw %s .
+    Wejdź i sprawdź https://slawekwitek.smallhost.pl/
+    Miłego dnia :)
+    Dostawę edytował użytkownik - %s .
+    """ % (dostawa_number, user_lastname, user_firstname)
     responder_email = os.environ.get('EMAIL_HOST_USER')
     admin_address = os.environ.get('ADMIN_ADDRES')
     email_two = user_email
